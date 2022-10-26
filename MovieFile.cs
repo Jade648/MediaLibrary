769737075746c2e9fd335a9a73a8b80761985a38
Jade 648 <maddiepeterson375@gmail.com>
2022-10-26 10:48:20 -0500

    Updated Media library Lab

--- MovieFile.cs
+++ MovieFile.cs
@@ -24,18 +24,19 @@ namespace MediaLibrary
             try
             {
                 StreamReader sr = new StreamReader(filePath);
+
                 while (!sr.EndOfStream)
                 {
                     // create instance of Movie class
                     Movie movie = new Movie();
                     string line = sr.ReadLine();
-                    // first look for quote(") in string
-                    // this indicates a comma(,) in movie title
+
+                      line = sr.ReadLine();
+                   
                     int idx = line.IndexOf('"');
                     if (idx == -1)
                     {
-                        // no quote = no comma in movie title
-                        // movie details are separated with comma(,)
+                    
                         string[] movieDetails = line.Split(',');
                         movie.mediaId = UInt64.Parse(movieDetails[0]);
                         movie.title = movieDetails[1];
@@ -45,29 +46,28 @@ namespace MediaLibrary
                     }
                     else
                     {
-                        // quote = comma or quotes in movie title
-                        // extract the movieId
+                        
                         movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
-                        // remove movieId and first comma from string
+                        
                         line = line.Substring(idx);
-                        // find the last quote
+                        
                         idx = line.LastIndexOf('"');
-                        // extract title
+                      
                         movie.title = line.Substring(0, idx + 1);
-                        // remove title and next comma from the string
+                   
                         line = line.Substring(idx + 2);
-                        // split the remaining string based on commas
+
                         string[] details = line.Split(',');
-                        // the first item in the array should be genres 
+                        
                         movie.genres = details[0].Split('|').ToList();
-                        // if there is another item in the array it should be director
+                        
                         movie.director = details[1];
-                        // if there is another item in the array it should be run time
+                        
                         movie.runningTime = TimeSpan.Parse(details[2]);
                     }
                     Movies.Add(movie);
                 }
-                // close file when done
+                
                 sr.Close();
                 logger.Info("Movies in file {Count}", Movies.Count);
             }
@@ -77,7 +77,7 @@ namespace MediaLibrary
             }
         }
 
-        // public method
+    
         public bool isUniqueTitle(string title)
         {
             if (Movies.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
@@ -89,20 +89,30 @@ namespace MediaLibrary
         }
 
         public void AddMovie(Movie movie)
+
         {
+
             try
             {
-                // first generate movie id
+                
                 movie.mediaId = Movies.Max(m => m.mediaId) + 1;
-                // if title contains a comma, wrap it in quotes
+
                 string title = movie.title.IndexOf(',') != -1 || movie.title.IndexOf('"') != -1 ? $"\"{movie.title}\"" : movie.title;
+                
                 StreamWriter sw = new StreamWriter(filePath, true);
-                // write movie data to file
+                
+                sw.WriteLine("(1) add a movie");
+
+                sw.WriteLine("(2) Display all Movies");
+
+                sw.WriteLine("Press Enter to Exit");
+
                 sw.WriteLine($"{movie.mediaId},{title},{string.Join("|", movie.genres)},{movie.director},{movie.runningTime}");
+                
                 sw.Close();
-                // add movie details to List
+                
                 Movies.Add(movie);
-                // log transaction
+                
                 logger.Info("Media id {Id} added", movie.mediaId);
             }
             catch (Exception ex)
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using NLog.Web;
 using System.IO;
+using System. Linq;
 
 namespace MediaLibrary
 {
@@ -14,10 +15,24 @@ namespace MediaLibrary
 
             logger.Info("Program started");
 
+            Console.WriteLine("1) Add a Movie");
+
+            Console.WriteLine("2) Display all Movies");
+
+            Console.WriteLine(" 3) Perform a search");
+
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
             MovieFile movieFile = new MovieFile(scrubbedFile);
 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            var Movies = movieFile.Movies.Where(m=>m.title.Contains("(1990)"));
+
+            Console.WriteLine($"There are {Movies.Count ()} movies from 1990");
+
+            Console.ForegroundColor = ConsoleColor.White;
+             
             logger.Info("Program ended");
         }
     }
